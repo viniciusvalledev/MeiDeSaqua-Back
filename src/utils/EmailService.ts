f773225c962b30d957cafba7529682992cfa9aec
Vinicius Valle Dev <vnthedeveloper@gmail.com>
2026-03-05 16:21:47 -0300

    fix: now email service esta atualizado

--- src/utils/EmailService.ts
+++ src/utils/EmailService.ts
@@ -13,11 +13,14 @@ class EmailService {
     this.transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
       port: Number(process.env.MAIL_PORT),
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
+      tls: {
+      rejectUnauthorized: false,
+    },
     });
   }
 
