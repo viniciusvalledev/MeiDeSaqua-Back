14e10f587c348841c15062e63c20ef420328d8d4
Vinícius Diller <viniciusdiller@hotmail.com>
2026-03-16 16:59:00 -0300

    feat: usuario pedir para reenviar email

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,13 +1,14 @@
-import { Router } from 'express';
-import AuthController from '../controllers/AuthController';
+import { Router } from "express";
+import AuthController from "../controllers/AuthController";
 
 const router = Router();
 
-router.post('/cadastro', AuthController.cadastrar);
-router.post('/login', AuthController.login);
-router.get('/confirm-account', AuthController.confirmAccount);
-router.post('/forgot-password', AuthController.forgotPassword);
-router.post('/reset-password', AuthController.resetPassword);
-router.get('/confirm-email-change', AuthController.confirmEmailChange);
+router.post("/cadastro", AuthController.cadastrar);
+router.post("/login", AuthController.login);
+router.get("/confirm-account", AuthController.confirmAccount);
+router.post("/resend-confirmation", AuthController.resendConfirmation);
+router.post("/forgot-password", AuthController.forgotPassword);
+router.post("/reset-password", AuthController.resetPassword);
+router.get("/confirm-email-change", AuthController.confirmEmailChange);
 
 export default router;
